e523ca7a0e19dd18f49991cb1014795fb82e86d3
Martin Escriba Bab <martin_escriba@hotmail.com>
2022-11-06 13:15:15 -0300

    primer commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import cors from "cors";
+import express from "express";
+import { initDB } from "./db/index.js";
+import ToDosRequestHandler from "./handlers/todos.js";
+
+
+const Api = express();
+
+Api.use(express.json());
+Api.use(express.urlencoded({ extended: false }));
+
+Api.use(cors());
+Api.use("/v1", ToDosRequestHandler);
+
+Api.listen(3000, () => {
+  console.log("API IS RUNNING");
+  initDB().then(() => {
+    console.log("DB IS READY");
+  });
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
